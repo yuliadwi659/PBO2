f028edf8f2f414d7f64f0a6ce2cf6fbcad148396
yuliadwi659 <32995672+yuliadwi659@users.noreply.github.com>
2017-10-27 20:36:38 +0800

    mengisi attribute dari object "m"

--- src/pbo2/PBO2.java
+++ src/pbo2/PBO2.java
@@ -2,6 +2,10 @@ package pbo2;
 public class PBO2 {
     public static void main(String[] args) {
        myFrame m = new myFrame();
+       
+       m.pesan = "Halo...";
+       m.bilBulat = 5;
+       m.bilDesimal = 3.14;
     }
     
 }
